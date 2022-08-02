c45dbaeccda09e9884e4e3b5cbc38f9a6d0f0a22
KyzenLFT <106551115+Trzy-Dev@users.noreply.github.com>
2022-08-02 23:34:56 +0100

    .

---
+++ [GRP-Sub]/GRP-Maps/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Galaxy
+
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Xonaa'
+
+file 'gabz_timecycle_mods_1.xml'
+data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
+
+this_is_a_map 'yes'
+client_script "@GRP-Anticheat/acloader.lua"
